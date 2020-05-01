466fb05b7ee08b5d7d7f0f411cf65bc462dc1d47
Rama638 <ramamohamad433@gmail.com>
2020-05-01 22:03:33 +0300

    solved toy propblem W4D6

--- W4D6/W4D6.js
+++ W4D6/W4D6.js
@@ -11,3 +11,12 @@ Another example could be:
 
 { name: "Gelato", countryOfOrigin: "Italy", flavor: "Chocolate Chip" }
 */
+function favoriteDessert(){
+	return {
+		name:"cake",
+		ovenTemp:"200",
+		prepTime:"45 minutes",
+		flavor:"vanilla",
+
+	}
+}
